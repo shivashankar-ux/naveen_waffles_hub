72b22e2f3d6113798d19e915fae6b82177404065
Shiva Shankar <shivashankar.7991@gmail.com>
2026-03-07 19:05:58 +0530

    add website files

---
+++ script.js
@@ -0,0 +1,6 @@
+function showTab(tab) {
+  document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
+  document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
+  document.getElementById('tab-' + tab).classList.add('active');
+  event.target.classList.add('active');
+}
